67a1ee6f622704a14514a0643ea98dc012b350d2
kenkeiras <kenkeiras@codigoparallevar.com>
2018-09-23 21:36:41 +0200

    Unescape </>/& on <pre> blocks.

--- scripts/markdown/markdown.sh
+++ scripts/markdown/markdown.sh
@@ -2,4 +2,4 @@
 
 DIR="$(dirname "$0")"
 
-sed -f "$DIR/markdown.sed" | tr -d '\n' | sed -f "$DIR/markdown2.sed" | sed -f "$DIR/markdown3.sed"
+awk -f "$DIR/escape_blocks.awk" | sed -f "$DIR/markdown.sed" | tr -d '\n' | sed -f "$DIR/markdown2.sed" | sed -f "$DIR/markdown3.sed"
